29ed67a17104886b02321a88f7230b4b66a53259
Ben <doc.ben.johnson@gmail.com>
2020-02-19 15:26:00 -0500

    made an assignment creation view, and a template for it

--- mysite/submitsys/urls.py
+++ mysite/submitsys/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('assignments', views.assignment_console, name='assignment_console'),
+    path('assignments/create', views.assignment_create, name='assignment_create'),
     path('<int:assignment_id>/', views.detail, name='detail'),
     path('<int:assignment_id>/student/', views.student, name='student'),
 ]
--- mysite/submitsys/views.py
+++ mysite/submitsys/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
-from .models import Assignment, Student, Submission
+from django.template import loader
 
 
 def index(request):
     return HttpResponse("Welcome to the UMBC CMSC Submit System!")
+
+
+def assignment_console(request):
+    context = {
+
+    }
+    template = loader.get_template('submitsys/assignment.html')
+    return HttpResponse(template.render(context, request))
+
+
+def assignment_create(request):
+    return HttpResponse("we did it:  " + request.POST['assignmentName'])
 
 
 def detail(request, assignment_id):
